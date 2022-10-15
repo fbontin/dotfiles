cacaf4b7ec6d3ec50e8634b88f10d424da4128cc
Filip Bontin <filip.bontin@devoteam.com>
2022-10-15 14:52:58 +0200

    Add scrollbar showing errors in file

---
+++ neovim/.config/nvim/lua/user/scrollbar.lua
@@ -0,0 +1,23 @@
+local colors_status_ok, colors = pcall(require, "tokyonight.colors")
+if not colors_status_ok then
+	return
+end
+
+local scrollbar_status_ok, scrollbar = pcall(require, "scrollbar")
+if not scrollbar_status_ok then
+	return
+end
+
+scrollbar.setup({
+	handle = {
+		color = colors.bg_highlight,
+	},
+	marks = {
+		Search = { color = colors.orange },
+		Error = { color = colors.error },
+		Warn = { color = colors.warning },
+		Info = { color = colors.info },
+		Hint = { color = colors.hint },
+		Misc = { color = colors.purple },
+	},
+})
