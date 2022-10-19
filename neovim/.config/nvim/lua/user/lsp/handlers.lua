623f4074380e3b7753b8f85d20607cc3324b2aa5
Filip Bontin <filip.bontin@devoteam.com>
2022-10-19 13:05:59 +0200

    Add command for triggering code actions

--- neovim/.config/nvim/lua/user/lsp/handlers.lua
+++ neovim/.config/nvim/lua/user/lsp/handlers.lua
@@ -68,14 +68,14 @@ local function lsp_keymaps(bufnr)
 	vim.api.nvim_buf_set_keymap(bufnr, "n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
 	-- vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
 	vim.api.nvim_buf_set_keymap(bufnr, "n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
-	-- vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
 	-- vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>f", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
 	vim.api.nvim_buf_set_keymap(bufnr, "n", "[d", '<cmd>lua vim.diagnostic.goto_prev({ border = "rounded" })<CR>', opts)
 	vim.api.nvim_buf_set_keymap(bufnr, "n", "gl", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
 	vim.api.nvim_buf_set_keymap(bufnr, "n", "]d", '<cmd>lua vim.diagnostic.goto_next({ border = "rounded" })<CR>', opts)
 	vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>q", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
+	vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
+
 	vim.cmd([[ command! Format execute 'lua vim.lsp.buf.format()' ]])
-	--  vim.cmd([[autocmd BufWritePre <buffer> vim.lsp.buf.format()]]) -- Auto format on save
 	vim.cmd([[autocmd BufWritePre * lua vim.lsp.buf.format({ bufnr = bufnr, timeout = 5000 }) ]]) -- Auto format on save
 end
 
