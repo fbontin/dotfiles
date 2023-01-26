5e8653710d4658e1e2cbbb97286bd9d936808e02
Filip Bontin <filip.bontin@devoteam.com>
2023-01-26 21:02:07 +0100

    Add plenary as subdependency to telescope

--- neovim/.config/nvim/lua/user/plugins/init.lua
+++ neovim/.config/nvim/lua/user/plugins/init.lua
@@ -1,5 +1,4 @@
 return {
-  "nvim-lua/plenary.nvim", -- Useful lua functions used ny lots of plugins
   "windwp/nvim-autopairs", -- Autopairs, integrates with both cmp and treesitter
 
   -- cmp plugins
--- neovim/.config/nvim/lua/user/plugins/telescope.lua
+++ neovim/.config/nvim/lua/user/plugins/telescope.lua
@@ -1,11 +1,14 @@
 return {
   "nvim-telescope/telescope.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
   config = function()
     require("telescope").setup({
       defaults = {
         prompt_prefix = " ",
         selection_caret = " ",
-        path_display = { "smart" }
+        path_display = { "smart" },
       },
     })
   end,
