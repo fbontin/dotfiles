4ff6928aa2d9721ff9f82fca678a5c01eb3387d2
Filip Bontin <filip.bontin@devoteam.com>
2023-01-29 12:07:24 +0100

    Cleanup cmp config for nvim

--- neovim/.config/nvim/lua/user/plugins/cmp.lua
+++ neovim/.config/nvim/lua/user/plugins/cmp.lua
@@ -52,16 +52,11 @@ return {
           luasnip.lsp_expand(args.body) -- For `luasnip` users.
         end,
       },
-      mapping = {
-        ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
-        ["<C-y>"] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
-        ["<C-e>"] = cmp.mapping({
-          i = cmp.mapping.abort(),
-          c = cmp.mapping.close(),
-        }),
+      mapping = cmp.mapping.preset.insert({
         -- Accept currently selected item. If none selected, `select` first item.
         -- Set `select` to `false` to only confirm explicitly selected items.
         ["<CR>"] = cmp.mapping.confirm({ select = true }),
+        -- Enable tabbing and shift-tabbing
         ["<Tab>"] = cmp.mapping(function(fallback)
           if cmp.visible() then
             cmp.select_next_item()
@@ -86,17 +81,12 @@ return {
           else
             fallback()
           end
-        end, {
-          "i",
-          "s",
-        }),
-      },
+        end, { "i", "s" }),
+      }),
       formatting = {
         fields = { "kind", "abbr", "menu" },
         format = function(entry, vim_item)
-          -- Kind icons
           vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
-          -- vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
           vim_item.menu = ({
             nvim_lsp = "[LSP]",
             nvim_lua = "[NVIM_LUA]",
@@ -113,14 +103,6 @@ return {
         { name = "luasnip" },
         { name = "buffer" },
         { name = "path" },
-      },
-      confirm_opts = {
-        behavior = cmp.ConfirmBehavior.Replace,
-        select = false,
-      },
-      experimental = {
-        ghost_text = false,
-        native_menu = false,
       },
     })
   end,
