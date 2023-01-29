81e70fd1551e188d31722a1c702e4b89c4e24d67
Filip Bontin <filip.bontin@devoteam.com>
2023-01-29 12:15:07 +0100

    Move null-ls to lazy.nvim setup

--- neovim/.config/nvim/lua/user/plugins/init.lua
+++ neovim/.config/nvim/lua/user/plugins/init.lua
@@ -7,7 +7,6 @@ return {
   "neovim/nvim-lspconfig", -- enable LSP
   "williamboman/mason.nvim",
   "williamboman/mason-lspconfig.nvim",
-  "jose-elias-alvarez/null-ls.nvim", -- for formatters and linters
   "tpope/vim-sleuth", -- fixes errors from .editorconfig, e.g. in Kompis
   "jose-elias-alvarez/typescript.nvim", -- for ts commands
 }
