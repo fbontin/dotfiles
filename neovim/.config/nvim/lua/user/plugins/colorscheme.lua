7133a3b491cc1e307fe01ebf84b7f7533f149a2e
Filip Bontin <filip.bontin@devoteam.com>
2023-02-21 13:31:22 +0100

    Add everforest as theme in light mode

--- neovim/.config/nvim/lua/user/plugins/colorscheme.lua
+++ neovim/.config/nvim/lua/user/plugins/colorscheme.lua
@@ -1,20 +1,22 @@
 return {
   "nyoom-engineering/oxocarbon.nvim",
   dependencies = {
-    "f-person/auto-dark-mode.nvim"
+    "f-person/auto-dark-mode.nvim",
+    "sainnhe/everforest",
   },
   priority = 1000,
   config = function()
-    local auto_dark_mode = require('auto-dark-mode')
+    local auto_dark_mode = require("auto-dark-mode")
     auto_dark_mode.setup({
       update_interval = 1000,
       set_dark_mode = function()
-        vim.api.nvim_set_option('background', 'dark')
-        vim.cmd('colorscheme oxocarbon')
+        vim.api.nvim_set_option("background", "dark")
+        vim.cmd("colorscheme oxocarbon")
       end,
       set_light_mode = function()
-        vim.api.nvim_set_option('background', 'light')
-        vim.cmd('colorscheme oxocarbon')
+        vim.api.nvim_set_option("background", "light")
+        -- vim.cmd('colorscheme oxocarbon')
+        vim.cmd("colorscheme everforest")
       end,
     })
     auto_dark_mode.init()
