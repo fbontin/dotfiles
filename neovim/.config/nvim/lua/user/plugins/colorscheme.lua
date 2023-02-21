2ebad12d85e0a40f1d6504d5dbd42564c65a931b
Filip Bontin <filip.bontin@devoteam.com>
2023-02-21 12:29:33 +0100

    Add nvim plugin for auto updating theme (light or dark)
    Based on current macos setting.

--- neovim/.config/nvim/lua/user/plugins/colorscheme.lua
+++ neovim/.config/nvim/lua/user/plugins/colorscheme.lua
@@ -1,8 +1,22 @@
 return {
   "nyoom-engineering/oxocarbon.nvim",
+  dependencies = {
+    "f-person/auto-dark-mode.nvim"
+  },
   priority = 1000,
   config = function()
-    vim.opt.background = "dark"
-    vim.cmd("colorscheme oxocarbon")
+    local auto_dark_mode = require('auto-dark-mode')
+    auto_dark_mode.setup({
+      update_interval = 1000,
+      set_dark_mode = function()
+        vim.api.nvim_set_option('background', 'dark')
+        vim.cmd('colorscheme oxocarbon')
+      end,
+      set_light_mode = function()
+        vim.api.nvim_set_option('background', 'light')
+        vim.cmd('colorscheme oxocarbon')
+      end,
+    })
+    auto_dark_mode.init()
   end,
 }
--- zsh/darkmode_toggle.zsh
+++ zsh/darkmode_toggle.zsh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+dark_or_light_mode() {
+  defaults read -g AppleInterfaceStyle 2> /dev/null || echo "Light"
+}
+
 # Toggles darkmode in system and terminal 
 darkmode() {
   # first toggle system dark/light mode
   $(osascript -e 'tell app "System Events" to tell appearance preferences to set dark mode to not dark mode')
 
   # then read what current/new is
-  CURR_MODE=$(defaults read -g AppleInterfaceStyle 2> /dev/null || echo "Light")
+  CURR_MODE=$(dark_or_light_mode)
 
   if [ "$CURR_MODE" = "Light" ]; then
     echo "Setting light"
