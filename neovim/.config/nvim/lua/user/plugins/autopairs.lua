ccc5194ab00be4af756a5e6ebab3e60be5401a11
Filip Bontin <filip.bontin@devoteam.com>
2023-02-26 15:28:02 +0100

    Replace autopairs with mini.pairs

--- neovim/.config/nvim/lua/user/plugins/autopairs.lua
+++ neovim/.config/nvim/lua/user/plugins/autopairs.lua
@@ -1,24 +1,7 @@
 return {
-  "windwp/nvim-autopairs",
+  "echasnovski/mini.nvim",
   event = "BufEnter",
   config = function()
-    require("nvim-autopairs").setup({
-      check_ts = true,
-      ts_config = {
-        lua = { "string", "source" },
-        javascript = { "string", "template_string" },
-      },
-      fast_wrap = {
-        pattern = string.gsub([[ [%'%"%)%>%]%)%}%,] ]], "%s+", ""),
-        end_key = "$",
-        keys = "qwertyuiopzxcvbnmasdfghjkl",
-        check_comma = true,
-        highlight = "PmenuSel",
-        highlight_grey = "LineNr",
-      },
-    })
-
-    local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-    require("cmp").event:on("confirm_done", cmp_autopairs.on_confirm_done())
+    require("mini.pairs").setup()
   end,
 }
