be48ce3c22b03d31d3f59eb7e2e48ddd3d9ba7d9
Filip Bontin <filip.bontin@devoteam.com>
2023-02-26 16:03:52 +0100

    Replace Comment.nvim with mini.comment

--- neovim/.config/nvim/lua/user/plugins/mini.lua
+++ neovim/.config/nvim/lua/user/plugins/mini.lua
@@ -1,8 +1,16 @@
 return {
   "echasnovski/mini.nvim",
   event = "BufEnter",
+  dependencies = { "JoosepAlviste/nvim-ts-context-commentstring", lazy = true },
   config = function()
     require("mini.pairs").setup()
     require("mini.statusline").setup()
+    require("mini.comment").setup({
+      hooks = {
+        pre = function()
+          require("ts_context_commentstring.internal").update_commentstring({})
+        end,
+      },
+    })
   end,
 }
