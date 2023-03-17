47db075e8be315673327ccc570c8a8026811835c
Filip Bontin <filip.bontin@devoteam.com>
2023-03-17 12:31:29 +0100

    Add live-grep-args plugin to telescope.
    To enable adding args to telescope live_grep search
    similar to how adding args to ripgrep works

--- neovim/.config/nvim/lua/user/plugins/telescope.lua
+++ neovim/.config/nvim/lua/user/plugins/telescope.lua
@@ -2,22 +2,25 @@ return {
   "nvim-telescope/telescope.nvim",
   dependencies = {
     "nvim-lua/plenary.nvim",
+    "nvim-telescope/telescope-live-grep-args.nvim",
   },
   event = "VeryLazy",
   config = function()
-    require("telescope").setup({
+    local telescope = require("telescope")
+    telescope.setup({
       defaults = {
         prompt_prefix = " ",
         selection_caret = " ",
         path_display = { "smart" },
       },
     })
+    telescope.load_extension("live_grep_args")
 
     vim.keymap.set(
       "n",
       "<leader>p",
       "<cmd>lua require'telescope.builtin'.find_files(require('telescope.themes').get_dropdown({ previewer = false }))<cr>"
     )
-    vim.keymap.set("n", "<leader>f", "<cmd>Telescope live_grep<cr>")
+    vim.keymap.set("n", "<leader>f", "<cmd>Telescope live_grep_args<cr>")
   end,
 }
