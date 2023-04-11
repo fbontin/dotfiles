b69423e04f028d290ef6555efda7e1a9dfd45557
Filip Bontin <fbontin@users.noreply.github.com>
2023-04-11 11:01:34 +0200

    Add shortcut to access recent files (leader f)

--- neovim/.config/nvim/lua/user/plugins/telescope.lua
+++ neovim/.config/nvim/lua/user/plugins/telescope.lua
@@ -22,5 +22,6 @@ return {
       "<cmd>lua require'telescope.builtin'.find_files(require('telescope.themes').get_dropdown({ previewer = false }))<cr>"
     )
     vim.keymap.set("n", "<leader>f", "<cmd>Telescope live_grep_args<cr>")
+    vim.keymap.set("n", "<leader>r", "<cmd>Telescope oldfiles<cr>")
   end,
 }
