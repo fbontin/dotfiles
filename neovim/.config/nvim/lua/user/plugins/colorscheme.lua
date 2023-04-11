2b63a7344fe494f78a44fffafacb80664ea88c19
Filip Bontin <fbontin@users.noreply.github.com>
2023-04-11 14:49:39 +0200

    Replace oxocarbon with moonfly theme

--- neovim/.config/nvim/lua/user/plugins/colorscheme.lua
+++ neovim/.config/nvim/lua/user/plugins/colorscheme.lua
@@ -1,21 +1,22 @@
 return {
-  "nyoom-engineering/oxocarbon.nvim",
+  "bluz71/vim-moonfly-colors",
+  name = "moonfly",
+  lazy = false,
+  priority = 1000,
   dependencies = {
     "f-person/auto-dark-mode.nvim",
     "sainnhe/everforest",
   },
-  priority = 1000,
   config = function()
     local auto_dark_mode = require("auto-dark-mode")
     auto_dark_mode.setup({
       update_interval = 1000,
       set_dark_mode = function()
         vim.api.nvim_set_option("background", "dark")
-        vim.cmd("colorscheme oxocarbon")
+        vim.cmd("colorscheme moonfly")
       end,
       set_light_mode = function()
         vim.api.nvim_set_option("background", "light")
-        -- vim.cmd('colorscheme oxocarbon')
         vim.cmd("colorscheme everforest")
       end,
     })
