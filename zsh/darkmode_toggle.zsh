20c783c23ceed4fb6e9de8bd81d3996a5a8f7fe3
Filip Bontin <fbontin@users.noreply.github.com>
2023-04-11 15:02:21 +0200

    Add "theme" function which takes either "dark" or "light"
    It changes between darkmode and lightmode

--- zsh/darkmode_toggle.zsh
+++ zsh/darkmode_toggle.zsh
@@ -14,10 +14,30 @@ darkmode() {
 
   # then set theme in kitty
   if [ "$CURR_MODE" = "Light" ]; then
-    echo "Setting light"
-    kitty @ set-colors -a -c "$HOME/.config/kitty/solarized-light-theme.conf"
+    set_light
   else 
     echo "Setting dark"
+    set_dark
+  fi
+}
+
+set_dark() {
+    $(osascript -e 'tell app "System Events" to tell appearance preferences to set dark mode to true')
     kitty @ set-colors -a -c "$HOME/.config/kitty/glacier-theme.conf"
+}
+
+set_light() {
+    $(osascript -e 'tell app "System Events" to tell appearance preferences to set dark mode to false')
+    kitty @ set-colors -a -c "$HOME/.config/kitty/solarized-light-theme.conf"
+}
+
+theme() {
+  if [ -z $1 ]; then 
+    # no parameter means it's just a toggle
+    darkmode
+  elif [[ $1 == "dark" ]]; then
+    set_dark
+  elif [[ $1 == "light" ]]; then
+    set_light
   fi
 }
