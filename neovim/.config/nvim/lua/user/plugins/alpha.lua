0d2d6c71c4c6f3115f24537eab4077a63d8ca8b0
Filip Bontin <fbontin@users.noreply.github.com>
2023-04-16 20:54:48 +0200

    Remove unused search shortcut from alpha

--- neovim/.config/nvim/lua/user/plugins/alpha.lua
+++ neovim/.config/nvim/lua/user/plugins/alpha.lua
@@ -18,7 +18,6 @@ return {
         dashboard.button("f", " " .. " Find file", ":Telescope find_files <CR>"),
         dashboard.button("e", " " .. " New file", ":ene <BAR> startinsert <CR>"),
         dashboard.button("r", " " .. " Recent files", ":Telescope oldfiles <CR>"),
-        dashboard.button("t", " " .. " Find text", ":Telescope live_grep <CR>"),
         dashboard.button("q", " " .. " Quit", ":qa<CR>"),
       },
     }
