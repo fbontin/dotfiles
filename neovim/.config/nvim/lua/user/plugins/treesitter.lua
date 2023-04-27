fb980490662211e4fd96c27b81a90e8f4d0651c0
Filip Bontin <fbontin@users.noreply.github.com>
2023-04-27 16:41:49 +0200

    Switch archived ts-rainbow to new version ts-rainbow2

--- neovim/.config/nvim/lua/user/plugins/treesitter.lua
+++ neovim/.config/nvim/lua/user/plugins/treesitter.lua
@@ -2,7 +2,7 @@ return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
   dependencies = {
-    "p00f/nvim-ts-rainbow",
+    "HiPhish/nvim-ts-rainbow2",
     "nvim-treesitter/nvim-treesitter-context",
   },
   event = "VeryLazy",
@@ -16,11 +16,7 @@ return {
         additional_vim_regex_highlighting = true,
       },
       indent = { enable = true, disable = { "yaml" } },
-      rainbow = {
-        enable = true,
-        extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
-        max_file_lines = nil, -- Do not enable for files with more than n lines, int
-      },
+      rainbow = { enable = true },
       context_commentstring = {
         -- needed for nvim-ts-context-commentstring
         enable = true,
