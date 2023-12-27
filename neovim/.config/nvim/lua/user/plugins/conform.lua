a67c1479f35a74d78b613703cb5a0f0b37ae6b16
Filip Bontin <fbontin@users.noreply.github.com>
2023-12-27 22:39:19 +0100

    Fix formatting for vue and through lsp

--- neovim/.config/nvim/lua/user/plugins/conform.lua
+++ neovim/.config/nvim/lua/user/plugins/conform.lua
@@ -1,12 +1,12 @@
 return {
   'stevearc/conform.nvim',
   opts = {
-    formatters_by_ft = {
-      rust = { "rust_analyzer" },
-      elixir = { "elixirls" },
-      lua = { "lua_ls" },
-      -- no js/ts formatting, that's handled by eslint through lsp, see lsp-zero.lua
-    },
-    format_on_save = { timeout_ms = 500, lsp_fallback = false }
+    format_on_save = function(bufnr)
+      if vim.bo[bufnr].filetype == 'vue' then
+        -- Avoid formatting vue files twice (due to EslintFixAll autocmd)
+        return { lsp_fallback = false }
+      end
+      return { lsp_fallback = true }
+    end,
   },
 }
