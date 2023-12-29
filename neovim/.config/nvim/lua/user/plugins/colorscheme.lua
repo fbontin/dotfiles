083d46a74fb77b95e23da5e6f3de0ff5d4aadd82
Filip Bontin <fbontin@users.noreply.github.com>
2023-12-29 20:55:15 +0100

    Replace moonfly with tokyonight as theme

--- neovim/.config/nvim/lua/user/plugins/colorscheme.lua
+++ neovim/.config/nvim/lua/user/plugins/colorscheme.lua
@@ -1,6 +1,5 @@
 return {
-  "bluz71/vim-moonfly-colors",
-  name = "moonfly",
+  "folke/tokyonight.nvim",
   lazy = false,
   priority = 1000,
   dependencies = {
@@ -9,12 +8,18 @@ return {
   },
   config = function()
     local auto_dark_mode = require("auto-dark-mode")
-    vim.g.moonflyTransparent = true
     auto_dark_mode.setup({
       update_interval = 1000,
       set_dark_mode = function()
+        require("tokyonight").setup({
+          style = "moon",
+          transparent = true,
+          on_colors = function(colors)
+            colors.comment = "#bbbbbb"
+          end
+        })
         vim.api.nvim_set_option("background", "dark")
-        vim.cmd("colorscheme moonfly")
+        vim.cmd("colorscheme tokyonight")
       end,
       set_light_mode = function()
         vim.api.nvim_set_option("background", "light")
