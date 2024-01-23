06cee0e3c09b80aaaa38ffa501bd5c6ac5c2e531
Filip Bontin <fbontin@users.noreply.github.com>
2024-01-23 13:52:37 +0100

    Add <leader>xw as a shortcut to open trouble.nvim for workspace

--- neovim/.config/nvim/lua/user/plugins/trouble.lua
+++ neovim/.config/nvim/lua/user/plugins/trouble.lua
@@ -3,5 +3,6 @@ return {
   dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
     vim.keymap.set("n", "<leader>xx", function() require("trouble").toggle() end)
+    vim.keymap.set("n", "<leader>xw", function() require("trouble").toggle("workspace_diagnostics") end)
   end
 }
