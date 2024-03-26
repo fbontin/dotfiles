95ebf6b0e43b757fb25035f3aa9b8934342e5f18
Filip Bontin <fbontin@users.noreply.github.com>
2024-03-26 07:01:09 +0100

    Add mini.pairs for auto pairs

--- neovim/.config/nvim/lua/user/plugins/mini.lua
+++ neovim/.config/nvim/lua/user/plugins/mini.lua
@@ -17,6 +17,7 @@ return {
     require("mini.bufremove").setup()
     require("mini.surround").setup()
     require("mini.files").setup()
+    require("mini.pairs").setup()
 
     -- Open file explorer on current file path
     vim.keymap.set("n", "<leader>e", "<cmd>lua MiniFiles.open(vim.api.nvim_buf_get_name(0)) <cr>")
