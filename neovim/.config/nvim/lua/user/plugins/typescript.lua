52b66a13a1cf9268aca40410e1343111d12fa856
Filip Bontin <fbontin@users.noreply.github.com>
2024-04-24 09:17:30 +0200

    Remove tsc.nvim and ts-error-translator
    I didn't use them

--- neovim/.config/nvim/lua/user/plugins/typescript.lua
+++
@@ -1,8 +0,0 @@
-return {
-  'dmmulroy/tsc.nvim',
-  dependencies = { 'dmmulroy/ts-error-translator.nvim' },
-  config = function()
-    require('tsc').setup()
-    require("ts-error-translator").setup()
-  end
-}
