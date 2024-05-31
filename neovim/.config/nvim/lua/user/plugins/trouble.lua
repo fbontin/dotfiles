54695449a43affc2f01f64384ef94c2e2944ccd0
Filip Bontin <fbontin@users.noreply.github.com>
2024-05-31 12:15:31 +0200

    Remove trouble plugin (I'm not really using it)

--- neovim/.config/nvim/lua/user/plugins/trouble.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "folke/trouble.nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  config = function()
-    vim.keymap.set("n", "<leader>xx", function() require("trouble").toggle() end)
-    vim.keymap.set("n", "<leader>xw", function() require("trouble").toggle("workspace_diagnostics") end)
-  end
-}
