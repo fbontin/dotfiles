e9ae09b4d49b9fec994363395cb9bd7971064ae5
Filip Bontin <fbontin@users.noreply.github.com>
2024-10-17 11:02:10 +0200

    Some more fish config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,2 +1,16 @@
 source $HOME/.config/fish/.aliases.fish
 
+export EDITOR="nvim"
+
+# NVM
+# function nvm
+# 	bass source ~/.nvm/nvm.sh --no-use ';' nvm $argv;
+# end
+# nvm use node > /dev/null 2>&1
+
+# Alstom
+source $HOME/repos/alstom/env.fish
+
+# Notes
+# source $HOME/repos/personal/notes/scripts/env.fish
+
