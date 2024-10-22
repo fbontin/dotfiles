b42930ee540309f63687461620a1a787987bb1cf
Filip Bontin <fbontin@users.noreply.github.com>
2024-10-22 13:25:51 +0200

    Set default fish node version to v20.17

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,11 +2,8 @@ source $HOME/.config/fish/.aliases.fish
 
 export EDITOR="nvim"
 
-# NVM
-# function nvm
-# 	bass source ~/.nvm/nvm.sh --no-use ';' nvm $argv;
-# end
-# nvm use node > /dev/null 2>&1
+# nvm
+set --universal nvm_default_version v20.17
 
 # Alstom
 source $HOME/repos/alstom/env.fish
